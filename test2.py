5d67051884b584cb5847e2dde1576e70a1aef72b
Yılmaz Alpaslan <ymzymz2007@gmail.com>
2021-06-04 22:25:58 +0300

    Version 0.3.3 Source Update

--- test2.py
+++ test2.py
@@ -14,13 +14,3 @@ key = key + '=='"""
 #print(base64.urlsafe_b64encode(base64.urlsafe_b64decode(bytes(key, 'utf-8'))).decode())
 
 
-key = ''
-for i in range(32):
-    choice = random.randint(1,32)
-    if choice < 25:
-        key = key + str(random.choice(string.ascii_letters))
-    elif choice >= 25 and choice < 30: 
-        key = key + str(random.choice(string.digits))
-    elif choice >= 30:
-        key = key + str(random.choice("!'^+%&/()=?_<>#${[]}\|__--$__--"))
-print(key)
